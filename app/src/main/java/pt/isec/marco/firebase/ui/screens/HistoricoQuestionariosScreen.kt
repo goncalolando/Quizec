a5cb129f3dc4e59afa418d4f53de4e302f31d579
renas420 <119908157+renas420@users.noreply.github.com>
2024-12-30 06:41:04 +0000

    Historico questionarios com cards

--- app/src/main/java/pt/isec/marco/firebase/ui/screens/HistoricoQuestionariosScreen.kt
+++ app/src/main/java/pt/isec/marco/firebase/ui/screens/HistoricoQuestionariosScreen.kt
@@ -1,6 +1,5 @@
 package pt.isec.marco.firebase.ui.screens
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,12 +22,29 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import pt.isec.marco.firebase.ui.viewmodels.FirebaseViewModel
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.rememberPagerState
+
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import pt.isec.marco.firebase.ui.viewmodels.Questionario
 
 @Composable
-fun HistoricoQuestionarioScreen(
+fun Card(
     viewModel: FirebaseViewModel,
-    navController: NavHostController,
-    showComplete: Boolean = false,
+    questionario: Questionario,
+    showComplete: Boolean
 ) {
     val questionariosAux by remember { viewModel.questionariosAux }
 
@@ -36,61 +52,176 @@ fun HistoricoQuestionarioScreen(
         viewModel.startQuestionariosObserver()
     }
 
-    if (questionariosAux.isEmpty()) {
-        Text("Nenhum questionário disponível")
-    } else {
-        LazyColumn {
-            items(questionariosAux) { questionario ->
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                    ) {
-                        // Aqui você pode adicionar o conteúdo que quiser dentro de cada Card
-                        Text(
-                            text = "Título do Questionário: ${questionario.descricao}",
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        var answer by remember { mutableStateOf<ShowAnswer?>(null) }
+    // Exibir o questionário se houver
+    if (questionario != null) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            elevation = CardDefaults.cardElevation(8.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(255, 224, 192))
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                // Título do Questionário
+                Text(
+                    text = "Título do Questionário: ${questionario.descricao}",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
 
-                        questionario.perguntas?.forEach { pergunta ->
-                            answer = when (pergunta.tipo) {
-                                "P01" -> {
-                                    when (pergunta.respostaCerta.getOrNull(0)) {
-                                        "true" -> ShowAnswer.BooleanAnswer(true)
-                                        "false" -> ShowAnswer.BooleanAnswer(false)
-                                        else -> ShowAnswer.NotAnswered
-                                    }
-                                }
-
-                                "P02" -> {
-                                    val respostaIndex =
-                                        pergunta.respostaCerta.getOrNull(0)?.toIntOrNull()
-                                    ShowAnswer.IntAnswer(respostaIndex)
-                                }
-
-                                else -> ShowAnswer.NotAnswered
-                            }
-                            Text("Pergunta: ${pergunta.titulo}")
-                            TipoPerguntaCard(
-                                pergunta = pergunta,
-                                showComplete = showComplete,
-                            )
-                        }
-                        // Aqui você pode adicionar mais campos ou lógica conforme necessário
-                        Spacer(modifier = Modifier.height(8.dp))
-                    }
+                // Se houver perguntas, renderize-as
+                questionario.perguntas?.forEach { pergunta ->
+                    Text("Pergunta: ${pergunta.titulo}")
+                    TipoPerguntaCard(
+                        pergunta = pergunta,
+                        showComplete = showComplete,
+                    )
                 }
+
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    } else {
+        // Caso não haja questionário para exibir
+        Text("Nenhum questionário disponível")
+    }
+}
+
+
+@Composable
+fun HistoricoQuestionarioScreen(
+    viewModel: FirebaseViewModel,
+    navController: NavHostController,
+    showComplete: Boolean
+) {
+    var selectedPage by remember { mutableStateOf(-1) }
+    val questionariosAux by remember { viewModel.questionariosAux }
+
+    LaunchedEffect(Unit) {
+        viewModel.startQuestionariosObserver()
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        val pagerState = rememberPagerState(pageCount = {
+            questionariosAux.size
+        })
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxSize()
+        ) { page ->
+            val pergunta = questionariosAux[page]
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(4.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .padding(2.dp)
+            ) {
+                Card (
+                    viewModel = viewModel,
+                    questionario = questionariosAux[page],
+                    showComplete = showComplete
+                )
+            }
+        }
+        Row(
+            Modifier
+                .align(Alignment.BottomCenter)
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color =
+                    if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(16.dp)
+                )
             }
         }
     }
-    }
+}
+
+//// TODO FINAL
+//// trocar o answers para dentro do composable
+//@Composable
+//fun Card(
+//    viewModel: FirebaseViewModel,
+//    navController: NavHostController,
+//    showComplete: Boolean = false,
+//) {
+//    val questionariosAux by remember { viewModel.questionariosAux }
+//
+//    LaunchedEffect(Unit) {
+//        viewModel.startQuestionariosObserver()
+//    }
+//
+//    if (questionariosAux.isEmpty()) {
+//        Text("Nenhum questionário disponível")
+//    } else {
+//        LazyColumn {
+//            items(questionariosAux) { questionario ->
+//                Card(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(16.dp),
+//                ) {
+//                    Column(
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(16.dp)
+//                    ) {
+//                        // Aqui você pode adicionar o conteúdo que quiser dentro de cada Card
+//                        Text(
+//                            text = "Título do Questionário: ${questionario.descricao}",
+//                            fontSize = 18.sp,
+//                            fontWeight = FontWeight.Bold
+//                        )
+//                        var answer by remember { mutableStateOf<ShowAnswer?>(null) }
+//
+//                        questionario.perguntas?.forEach { pergunta ->
+//                            answer = when (pergunta.tipo) {
+//                                "P01" -> {
+//                                    when (pergunta.respostaCerta.getOrNull(0)) {
+//                                        "true" -> ShowAnswer.BooleanAnswer(true)
+//                                        "false" -> ShowAnswer.BooleanAnswer(false)
+//                                        else -> ShowAnswer.NotAnswered
+//                                    }
+//                                }
+//
+//                                "P02" -> {
+//                                    val respostaIndex =
+//                                        pergunta.respostaCerta.getOrNull(0)?.toIntOrNull()
+//                                    ShowAnswer.IntAnswer(respostaIndex)
+//                                }
+//
+//                                else -> ShowAnswer.NotAnswered
+//                            }
+//                            Text("Pergunta: ${pergunta.titulo}")
+//                            TipoPerguntaCard(
+//                                pergunta = pergunta,
+//                                showComplete = showComplete,
+//                            )
+//                        }
+//                        // Aqui você pode adicionar mais campos ou lógica conforme necessário
+//                        Spacer(modifier = Modifier.height(8.dp))
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    }
 //    val questionariosAux by remember { viewModel.questionariosAux }
 //
 //    LaunchedEffect(Unit) {
